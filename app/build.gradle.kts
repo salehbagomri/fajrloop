f861effb2102709f20be0f91e95164d45eab9b89
salehbagomri <s.bagomri@gmail.com>
2026-08-18 23:52:34 +0300

    release: bump versionCode to 4 (v1.0.4)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.bagomri.fajrloop"
         minSdk = 26
         targetSdk = 36
-        versionCode = 3
-        versionName = "1.0.3"
+        versionCode = 4
+        versionName = "1.0.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
